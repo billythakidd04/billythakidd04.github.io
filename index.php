ee241ab8207db7f318603e05fcc6bf855fd6987b
Bill Caffery <billythakidd04@gmail.com>
2014-05-06 21:34:52 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Grown Kidd Creations</title>
+		<link rel="stylesheet" href="css/main.css">
+		<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+			});
+		</script>
+	</head>
+	<body>
+		<header>
+			<img alt="Picture of me and Cian" src="img/caindaddysleepin.jpeg" title="Cian and Daddy">
+			<?include_once 'nav/nav.php';?>
+		</header>
+		<div id="container">
+			<aside class="left"><p>No idea what to put here. how about you fill out the form <a href="pages/sitepage.php" target="new">here</a> and tell me?</p></aside>
+			<div id="mainContent">Test</div>
+			<aside class="right">
+				Recent News/Updates:<br/>
+				<span class="undercon">This will be updated soon with links and maybe a <a href="pages/viewblog.php">blog</a>.</span>
+				<ul>
+					<li>Cian</li>
+					<li>Work</li>
+					<li>Family</li>
+				</ul>
+			</aside>
+		</div>
+		<footer>
+			Send me some love at <a href="mailto:billythakidd04@gmail.com">BillyThaKidd04@gmail.com</a><br/>
+			Or find me at:<br/>
+			<div class="g-plus" data-href="https://plus.google.com/109325835178774768962?prsrc=3" data-theme="dark" rel="author"></div>
+			<a href="https://twitter.com/ThaKidd04" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @ThaKidd04</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</footer>
+	</body>
+</html>
